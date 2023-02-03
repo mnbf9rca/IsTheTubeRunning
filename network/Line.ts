baf08aae8d374bbd7ffbbccd77515fff80cfe1f1
Rob Aleck <mnbf9rca@users.noreply.github.com>
2023-02-03 22:25:33 +0000

    created Line, LineSegment, Mode, Stoppoint

--- network/Line.ts
+++ network/Line.ts
@@ -1,162 +1,77 @@
-import Stoppoint from './Stoppoint';
+import Mode from './Mode';
 
-enum tfl_line  {
-  bakerloo = 'Bakerloo',
-  central = 'Central',
-  circle = 'Circle',
-  district = 'district',
-  dlr = 'dlr',
-  elizabethline = 'elizabeth-line',
-  hammersmithcity = 'hammersmith-city',
-  jubilee = 'jubilee',
-  metropolitan = 'metropolitan',
-  northern = 'northern',
-  overground = 'overground',
-  piccadilly = 'piccadilly',
-  victoria = 'victoria',
-  waterloocity = 'waterloo-city',
-  tube = 'tube',
+const tfl_lines: { [key: string]: string } = {
+  bakerloo: 'Bakerloo',
+  central: 'Central',
+  circle: 'Circle',
+  district: 'District',
+  dlr: 'DLR',
+  'elizabeth-line': 'Elizabeth Line',
+  'hammersmith-city': 'Hammersmith & City',
+  jubilee: 'Jubilee',
+  metropolitan: 'Metropolitan',
+  northern: 'Northern',
+  piccadilly: 'Piccadilly',
+  victoria: 'Victoria',
+  'waterloo-city': 'Waterloo & City',
+  'london-overground': 'London Overground',
 }
 
-interface ILine {
+export interface ILine {
   toString: () => string;
-  lineName: tfl_line;
-  [Symbol.toStringTag]?: string;
+  toObject: () => ILineObject;
+  lineName: string;
+  displayName: string;
+  mode: Mode;
+  [Symbol.toStringTag]: string;
 }
 
-interface ILineSegment extends ILine {
-  id: string;
-  type: "line-segment";
-  from?: Stoppoint;
-  fromId: string;
-  to?: Stoppoint;
-  toId: string;
-  branchId: number;
-  direction: "inbound" | "outbound";
-  label?: string;
-  fromObject?: (obj: any) => ILineSegment;
+export interface ILineObject {
+  lineName: string;
+  displayName: string;
+  mode: string;
+  type: string;
 }
-class Line implements ILine{
-  private _name: tfl_line;
 
-  constructor(lineName: string) {
-    this._name = lineName as tfl_line;
-  }
-  toString(): string {
-    return String(this._name);
-  }
-  get lineName(): tfl_line {
-    return this._name;
-  }
-
-  get [Symbol.toStringTag](): string {
-    return `Line: ${this._name}`;
-  }
-
-} 
-
-class LineSegment extends Line implements ILineSegment {
-  private _id: string;
-  private _type: "line-segment";
-  private _from?: Stoppoint;
-  private _fromId: string;
-  private _to?: Stoppoint;
-  private _toId: string;
-  private _branchId: number;
-  private _direction: "inbound" | "outbound";
-  private _label?: string;
-  constructor (
-    lineName: string,
-    id: string,
-    branchId: number,
-    direction: "inbound" | "outbound",
-    fromId?: string,
-    toId?: string,    
-    from?: Stoppoint,
-    to?: Stoppoint,
-    label?: string,
-  ) {
-    if (fromId === undefined && from === undefined) {
-      throw new Error("If fromId is undefined, from must be defined");
-    } else {
-      fromId = fromId || from!.id;
+export default class Line implements ILine {
+  private _lineName: string;
+  private _displayName: string;
+  private _mode: Mode;
+  public constructor(lineName: string, mode: Mode) {
+    if (!tfl_lines[lineName]) {
+      throw new Error(`Line ${lineName} not found in tfl_lines`);
     }
-    if (toId === undefined && to === undefined) {
-      throw new Error("If toId is undefined, to must be defined");
-    } else {
-      toId = toId || to!.id;
-    }
-    super(lineName),
-    this._id = id;
-    this._type = "line-segment";
-    this._from = from;
-    this._fromId = fromId;
-    this._to = to;
-    this._toId = toId;
-    this._branchId = branchId;
-    this._direction = direction;
-    this._label = label;
+    this._lineName = lineName;
+    this._displayName = tfl_lines[lineName];
+    this._mode = mode;
   }
-  get id(): string {
-    return this._id;
+
+  public toString(): string {
+    return String(this._lineName);
   }
-  get type(): "line-segment" {
-    return this._type;
+
+  public get lineName(): string {
+    return this._lineName;
   }
-  get from(): Stoppoint | undefined {
-    return this._from;
+  
+  public get displayName(): string {
+    return this._displayName;
   }
-  get fromId(): string {
-    return this._fromId;
+
+  public get mode(): Mode {
+    return this._mode;
   }
-  get to(): Stoppoint | undefined {
-    return this._to;
-  }
-  get toId(): string {
-    return this._toId;
-  }
-  get branchId(): number {
-    return this._branchId;
-  }
-  get direction(): "inbound" | "outbound" {
-    return this._direction;
-  }
-  get label(): string | undefined {
-    return this._label;
-  }
-  toString(): string {
-    return `${this._id}`
-  }
-  static fromObject(obj: any): ILineSegment {
-    return new LineSegment(
-      obj.lineName,
-      obj.id,
-      obj.branchId,
-      obj.direction,
-      obj.fromId,
-      obj.toId,
-      obj.from,
-      obj.to,
-      obj.label,
-    );
-  }
-  getObject() {
+
+  public toObject(): ILineObject {
     return {
-      lineName: super.lineName.toString(),
-      id: this._id,
-      type: this._type,
-      from: this._from,
-      fromId: this._fromId,
-      to: this._to,
-      toId: this._toId,
-      branchId: this._branchId,
-      direction: this._direction,
-      label: this._label,
+      lineName: this._lineName,
+      displayName: this._displayName,
+      mode: this._mode.id,
+      type: 'line',
     };
   }
+
+  public get [Symbol.toStringTag](): string {
+    return `Line: ${this._lineName}`;
+  }
 }
-
-
-
-
-export { Line, LineSegment };
--- network/Mode.ts
+++ network/Mode.ts
@@ -1,7 +1,60 @@
-export enum Mode {
-  bus = 'bus',
-  dlr = 'dlr',
-  'elizabeth-line' = 'elizabeth-line',
-  overground = 'overground',
-  tube = 'tube'
+const Modes: { [key: string]: string } = {
+  bus: 'bus',
+  dlr: 'DLR',
+  'elizabeth-line': 'Elizabeth Line',
+  overground: 'Overground',
+  tube: 'Tube'
+}
+
+
+interface IMode {
+  id: string;
+  name: string;
+  toObject: () => IModeObject;
+  toString: () => string;
+  [Symbol.toStringTag]: string;
+
+}
+
+export interface IModeObject {
+  type: string;
+  id: string;
+  name: string;
+}
+
+export default class Mode implements IMode {
+  private _id: "bus" | "dlr" | "elizabeth-line" | "overground" | "tube";;
+  private _name: string;
+
+  public constructor(mode: "bus" | "dlr" | "elizabeth-line" | "overground" | "tube") {
+    if (!Object.keys(Modes).includes(mode)) {
+      throw new Error(`Invalid mode: ${mode}`);
+    }
+    this._id = mode
+    this._name = Modes[mode]
+  }
+
+  public get id(): string {
+    return String(this._id);
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public toString(): string {
+    return String(this._id);
+  }
+
+  public toObject(): IModeObject {
+    return {
+      type: 'mode',
+      id: this._id,
+      name: this._name,
+    };
+  }
+  
+  public get [Symbol.toStringTag](): string {
+    return `Mode: ${this._id}`;
+  }
 }
