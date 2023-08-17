44ec5d11c7c6aaccb3927ea0257e9895cfa98cbd
Rob Aleck <mnbf9rca@users.noreply.github.com>
2023-08-17 11:34:21 +0800

    add error handling to zod type generation

--- scripts/generate-zod-schemas.js
+++ scripts/generate-zod-schemas.js
@@ -1,10 +1,16 @@
 const { execSync } = require('child_process')
 const config = require('../ts-to-zod.config.js')
 
-config.forEach(entry => {
-  const command = `npx ts-to-zod --config ${entry.name}`
-  console.log(`Running: ${command}`)
-  execSync(command, { stdio: 'inherit' })
-})
+try {
+  config.forEach(entry => {
+    const command = `npx ts-to-zod --config ${entry.name}`
+    console.log(`Running: ${command}`)
+    execSync(command, { stdio: 'inherit' })
+  })
 
-console.log('All commands executed successfully.')
+  console.log('All commands executed successfully.')
+  process.exit(0) // Exit with success status code
+} catch (error) {
+  console.error('An error occurred while executing the commands:', error.message)
+  process.exit(1) // Exit with failure status code
+}
