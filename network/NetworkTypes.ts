23df93b9ec84ce4d737a7e4bf2c560b4d87f541e
Rob Aleck <mnbf9rca@users.noreply.github.com>
2023-08-17 11:50:27 +0800

    add network types for line, linesegment, enum mode

--- network/NetworkTypes.ts
+++ network/NetworkTypes.ts
@@ -5,7 +5,28 @@ export interface StopPoint {
   naptanId: string;
   lat: number | string;
   lon: number | string;
-  modes: string[];
+  modes: Mode[];
   lines: string[];
 }
 
+export interface Line {
+  type: string;
+  lineName: string;
+  displayName: string;
+  mode?: Mode;
+}
+
+export interface LineSegment {
+  type: string;
+  line: string;
+  from: string;
+  to: string;
+}
+
+export enum Mode {
+  bus = 'bus',
+  dlr = 'dlr',
+  elizabethline = 'elizabeth-line', // Changed key from elizabeth-line
+  overground = 'overground',
+  tube = 'tube',
+}
